068a316ac36582198bf02f3489a91946e326c1b9
Jaron <jingyang0608@outlook.com>
2021-04-03 11:07:26 +0800

    add

--- note/面试题17.12.BiNode.java
+++ note/面试题17.12.BiNode.java
@@ -1,23 +1,52 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+// 递归
 class Solution {
+    TreeNode dummy = new TreeNode(-1);
+    TreeNode prev = dummy;
+
+    public TreeNode convertBiNode(TreeNode root) {
+        inorder(root);
+        return dummy.right;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        prev.right = root;
+        prev = root;
+        prev.left = null;
+        inorder(root.right);
+    }
+}
+
+// 非递归
+class Solution1 {
+    TreeNode dummy = new TreeNode(-1);
+    TreeNode prev = dummy;
+
     public TreeNode convertBiNode(TreeNode root) {
         if (root == null) {
             return null;
         }
-        TreeNode l = convertBiNode(root.left);
-        TreeNode r = convertBiNode(root.right);
-        root.left = null;
-
-        if (l == null) {
-            root.right = r;
-            return root;
-        } else {
-            TreeNode p = l;
-            while (p.right != null) {
-                p = p.right;
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode ptr = root;
+        // 栈空的时候可能还有右子树没有处理，所以得判断ptr
+        while (ptr != null || !stack.isEmpty()) {
+            if (ptr == null) {
+                ptr = stack.pop();
+                ptr.left = null;
+                prev.right = ptr;
+                prev = ptr;
+                ptr = ptr.right;
+            } else {
+                stack.push(ptr);
+                ptr = ptr.left;
             }
-            p.right = root;
-            root.right = r;
-            return l;
         }
+        return dummy.right;
     }
 }
